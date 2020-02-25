05ad0ba6c7ae2b067b4bc4d417084e898cd2f6cf
Joseph M. Keller <josephkeller1999@icloud.com>
2020-02-24 23:49:43 -0600

    Initial commit

---
+++ ChannelPointsSFX/frmMain.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ChannelPointsSFX
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trkVolume = new System.Windows.Forms.TrackBar();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblLine = new System.Windows.Forms.Label();
+            this.lstbxSoundsRewards = new System.Windows.Forms.ListBox();
+            this.lstbxSoundsPaths = new System.Windows.Forms.ListBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trkVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trkVolume
+            // 
+            this.trkVolume.Location = new System.Drawing.Point(12, 171);
+            this.trkVolume.Maximum = 100;
+            this.trkVolume.Name = "trkVolume";
+            this.trkVolume.Size = new System.Drawing.Size(271, 45);
+            this.trkVolume.TabIndex = 0;
+            this.trkVolume.Scroll += new System.EventHandler(this.trkVolume_Scroll);
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(289, 171);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.ReadOnly = true;
+            this.txtVolume.Size = new System.Drawing.Size(28, 20);
+            this.txtVolume.TabIndex = 1;
+            this.txtVolume.Text = "100";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(12, 155);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(42, 13);
+            this.lblVolume.TabIndex = 2;
+            this.lblVolume.Text = "Volume";
+            // 
+            // lblLine
+            // 
+            this.lblLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLine.Location = new System.Drawing.Point(60, 162);
+            this.lblLine.Name = "lblLine";
+            this.lblLine.Size = new System.Drawing.Size(257, 2);
+            this.lblLine.TabIndex = 3;
+            // 
+            // lstbxSoundsRewards
+            // 
+            this.lstbxSoundsRewards.FormattingEnabled = true;
+            this.lstbxSoundsRewards.Items.AddRange(new object[] {
+            "Highlight my Message",
+            "Modify a Single Emote",
+            "Unlock a Random Sub Emote",
+            "Choose an emote to unlock",
+            "T1 Sub"});
+            this.lstbxSoundsRewards.Location = new System.Drawing.Point(15, 13);
+            this.lstbxSoundsRewards.Name = "lstbxSoundsRewards";
+            this.lstbxSoundsRewards.Size = new System.Drawing.Size(136, 95);
+            this.lstbxSoundsRewards.TabIndex = 4;
+            this.lstbxSoundsRewards.SelectedIndexChanged += new System.EventHandler(this.lstbxSoundsRewards_SelectedIndexChanged);
+            // 
+            // lstbxSoundsPaths
+            // 
+            this.lstbxSoundsPaths.FormattingEnabled = true;
+            this.lstbxSoundsPaths.Items.AddRange(new object[] {
+            "C:\\test1",
+            "C:\\test2",
+            "C:\\test3",
+            "C:\\test4",
+            "C:\\test5"});
+            this.lstbxSoundsPaths.Location = new System.Drawing.Point(151, 13);
+            this.lstbxSoundsPaths.Name = "lstbxSoundsPaths";
+            this.lstbxSoundsPaths.Size = new System.Drawing.Size(139, 95);
+            this.lstbxSoundsPaths.TabIndex = 5;
+            this.lstbxSoundsPaths.SelectedIndexChanged += new System.EventHandler(this.lstbxSoundsPaths_SelectedIndexChanged);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(296, 13);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(21, 23);
+            this.btnUp.TabIndex = 6;
+            this.btnUp.Text = "∧";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(296, 85);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(21, 23);
+            this.btnDown.TabIndex = 7;
+            this.btnDown.Text = "∨";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 114);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add Sound";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(113, 114);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(80, 23);
+            this.btnTest.TabIndex = 9;
+            this.btnTest.Text = "Test Sound";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(210, 114);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(80, 23);
+            this.btnRemove.TabIndex = 10;
+            this.btnRemove.Text = "Delete Sound";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 228);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.lstbxSoundsPaths);
+            this.Controls.Add(this.lstbxSoundsRewards);
+            this.Controls.Add(this.lblLine);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.trkVolume);
+            this.MaximumSize = new System.Drawing.Size(341, 267);
+            this.MinimumSize = new System.Drawing.Size(341, 267);
+            this.Name = "frmMain";
+            this.Text = "Dude22072\'s Channel Points SFX Program";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trkVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trkVolume;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblLine;
+        private System.Windows.Forms.ListBox lstbxSoundsRewards;
+        private System.Windows.Forms.ListBox lstbxSoundsPaths;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ ChannelPointsSFX/frmMain.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Windows.Forms;
+using TwitchLib.PubSub;
+using TwitchLib.PubSub.Events;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.IO;
+
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+namespace ChannelPointsSFX
+{
+    public partial class frmMain : Form
+    {
+        private static TwitchPubSub client;
+        private static Dictionary<string, string> bindings = new Dictionary<string, string>();
+        private static MediaPlayer thePlayer;
+        private int volumeLevel = 50; 
+        private int boxSelection = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+            client = new TwitchPubSub();
+
+            client.OnPubSubServiceConnected += onPubSubServiceConnected;
+            client.OnListenResponse += onListenResponse;
+            client.OnRewardRedeemed += OnRewardRedeemed;
+
+            client.ListenToRewards("");
+
+            client.Connect();
+
+            if(File.Exists("settings.txt"))
+            {
+                String[] loadSettings = File.ReadAllLines("settings.txt");
+                foreach(string line in loadSettings)
+                {
+                    string[] split = line.Split('|');
+                    bindings.Add(split[0], split[1]);
+                }
+            }
+
+            reloadListItems();
+
+            txtVolume.Text = volumeLevel.ToString();
+            trkVolume.Value = volumeLevel;
+        }
+
+        private static void onPubSubServiceConnected(object sender, EventArgs e)
+        {
+            // SendTopics accepts an oauth optionally, which is necessary for some topics
+            client.SendTopics();
+        }
+
+        private static void onListenResponse(object sender, OnListenResponseArgs e)
+        {
+            if (!e.Successful)
+                throw new Exception($"Failed to listen! Response: {e.Response}");
+        }
+
+        private void OnRewardRedeemed(object sender, OnRewardRedeemedArgs e)
+        {
+#if DEBUG
+            Debug.WriteLine("Reward Redeemed:");
+            Debug.WriteLine("\tTitle:"+e.RewardTitle);
+            Debug.WriteLine("\tPrompt:"+e.RewardPrompt);
+            Debug.WriteLine("\tUser:" + e.DisplayName);
+            Debug.WriteLine("----------------");
+#endif
+            if(bindings.ContainsKey(e.RewardTitle))
+            {
+                string output;
+                bindings.TryGetValue(e.RewardTitle, out output);
+#if DEBUG
+                Debug.WriteLine(output);
+#endif
+                this.PlaySound(output);
+            }
+        }
+
+        private void trkVolume_Scroll(object sender, EventArgs e)
+        {
+            this.volumeLevel = trkVolume.Value;
+            txtVolume.Text = this.volumeLevel.ToString();
+            if (thePlayer != null)
+            {
+                this.SetVolume(this.volumeLevel);
+            }
+        }
+
+        // `volume` is assumed to be between 0 and 100.
+        public void SetVolume(int volume)
+        {
+            // MediaPlayer volume is a float value between 0 and 1.
+            thePlayer.Volume = volume / 100.0f;
+        }
+
+        public void PlaySound(string filename)
+        {
+#if DEBUG
+            Debug.WriteLine("Playing sound from \"" + filename + "\".");
+#endif
+            thePlayer = new MediaPlayer();
+            thePlayer.Open(new Uri(filename));
+            this.SetVolume(this.volumeLevel);
+            thePlayer.MediaEnded += ThePlayer_MediaEnded;
+            thePlayer.Play();
+            
+        }
+
+        private void ThePlayer_MediaEnded(object sender, EventArgs e)
+        {
+#if DEBUG
+            Debug.WriteLine("Sound playing ended.");
+#endif
+            thePlayer.Close();
+        }
+
+        private void reloadListItems()
+        {
+            lstbxSoundsRewards.Items.Clear();
+            lstbxSoundsPaths.Items.Clear();
+
+            foreach (KeyValuePair<string, string> kvp in bindings)
+            {
+                lstbxSoundsRewards.Items.Add(kvp.Key);
+                string[] val = kvp.Value.Split('\\');
+                lstbxSoundsPaths.Items.Add(val[val.Length-1]);
+            }
+
+            if (boxSelection > (bindings.Count - 1)) boxSelection = (bindings.Count - 1);
+
+            if(bindings.Count == 0)
+            {
+                btnUp.Enabled = false;
+                btnDown.Enabled = false;
+                btnRemove.Enabled = false;
+                btnTest.Enabled = false;
+            } 
+            else
+            {
+                btnUp.Enabled = true;
+                btnDown.Enabled = true;
+                btnRemove.Enabled = true;
+                btnTest.Enabled = true;
+                lstbxSoundsRewards.SelectedIndex = boxSelection;
+                lstbxSoundsPaths.SelectedIndex = boxSelection;
+                saveBindings();
+            }
+        }
+
+        private void saveBindings()
+        {
+            String buildString = "";
+            foreach (KeyValuePair<string, string> kvp in bindings)
+            {
+                buildString += kvp.Key + "|" + kvp.Value + "\n";
+            }
+            File.WriteAllText("settings.txt", buildString);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            boxSelection--;
+            if (boxSelection < 0) boxSelection = 0;
+            lstbxSoundsRewards.SelectedIndex = boxSelection;
+            lstbxSoundsPaths.SelectedIndex = boxSelection;
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            boxSelection++;
+            if (boxSelection > (bindings.Count-1)) boxSelection = (bindings.Count - 1);
+            lstbxSoundsRewards.SelectedIndex = boxSelection;
+            lstbxSoundsPaths.SelectedIndex = boxSelection;
+        }
+
+        private void lstbxSoundsRewards_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstbxSoundsRewards.SelectedIndex = boxSelection;
+        }
+
+        private void lstbxSoundsPaths_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstbxSoundsPaths.SelectedIndex = boxSelection;
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            bindings.Remove(lstbxSoundsRewards.SelectedItem.ToString());
+            reloadListItems();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string output;
+            bindings.TryGetValue(lstbxSoundsRewards.SelectedItem.ToString(), out output);
+            PlaySound(output);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string rewardName = Prompt.ShowDialog("Enter the twitch Channel Points reward name EXACTLY as it is on twitch.", "Channel Reward Name");
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    string filePath = openFileDialog.FileName;
+                    bindings.Add(rewardName, filePath);
+                    reloadListItems();
+                }
+            }
+        }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, AutoSize = true, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
